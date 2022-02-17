6ff0bd462710798ddf8faefc8a700b97d75c850f
hfdzafrnsyh <hafidzafrnsyh@outlook.co.id>
2022-02-17 16:51:28 +0700

    edit user table

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,7 +15,12 @@
             </div>
 
             <a href="/exportuser" class="btn btn-success btn-sm mb-3" target="_blank">Export Excel</a>
-            <a href="/importuser" class="btn btn-primary btn-sm mb-3" target="_blank">Import From Excel</a>
+            <!-- Button trigger modal -->
+                {{-- <button type="button" class="btn btn-primary btn-sm mb-3" data-toggle="modal" data-target="#exampleModal">
+                        Import from Excel
+                </button> --}}
+  
+  
         
             <table class="table table-striped table-dark">
                     <thead>
@@ -60,6 +65,34 @@
    
     
 </div>
+
+{{-- 
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Import File</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form action="#" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                  <label for="exampleFormControlFile1">Input File</label>
+                  <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                </div>
+              </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div> --}}
 
 
 
